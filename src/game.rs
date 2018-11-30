b0bae6782424451baace1a6c30429faf586215dd
Tomas Gavenciak <gavento@ucw.cz>
2018-11-30 17:15:49 +0100

    Implement wrrapped API draft

--- src/game.rs
+++ src/game.rs
@@ -1,82 +1,154 @@
 use std::fmt::Debug;
 use std::hash::Hash;
+use std::borrow::Cow;
 
-use crate::{ActionIndex, ActivePlayer, History, HistoryInfo, PlayerObservation};
+use crate::{ActionIndex, Categorical, Utility};
 
 pub trait Game: Debug + Clone {
-    type StateData: Clone + Default + Debug;
+    type State: Clone + Debug;
     type Observation: Clone + Debug + Hash + PartialEq + Eq;
+    type Action: Clone + Debug + Hash + PartialEq + Eq;
 
-    fn new_history(&self, with_observations: bool) -> HistoryInfo<Self> {
-        HistoryInfo::new(self, with_observations)
+    /// TO BE IMPLEMENTED BY PLAYER:
+    fn players(&self) -> usize;
+    fn initial_state(&self) -> (Self::State, ActivePlayer<Self>);
+    /// TODO: Give the prev_active?
+    /// TODO: Give prev_history and action separately?
+    fn update_state(&self, state: &Self::State, history: &Vec<Self::Action>, prev_active: &ActivePlayer<Self>) -> 
+        (Self::State, ActivePlayer<Self>, Vec<Option<Self::Observation>>);
+
+    fn start(&self) -> HistoryInfo<Self> {
+        let (state, active) = self.initial_state();
+        HistoryInfo::new(self, state, active)
     }
 
-    fn players(&self) -> u32 {
-        2
+    fn play_value(&self, hist: &HistoryInfo<Self>, action: &Self::Action) -> HistoryInfo<Self> {
+        if let ActivePlayer::Terminal(_) = hist.active {
+            panic!("playing in terminal state {:?}", hist);
+        }
+        match hist.active.actions().iter().position(|x| x == action) {
+            Some(idx) => self.play(&hist, idx),
+            None => panic!("action {:?} not available in state {:?}", action, hist)
+        }
     }
 
-    fn action_name(&self, history: &HistoryInfo<Self>, action: ActionIndex) -> String {
-        action.to_string()
-    }
-
-    fn play(&self, hinfo: &HistoryInfo<Self>, action: ActionIndex) -> HistoryInfo<Self> {
-        println!("{:?} {:?}", hinfo, action);
-        match hinfo.active {
-            ActivePlayer::Player(p, ref actions) => debug_assert!(actions.contains(&action)),
-            ActivePlayer::Chance(ref dist) => {
-                debug_assert!(dist.items().contains(&action));
-            }
-            ActivePlayer::Terminal(_) => {
-                panic!("Playing {:?} in terminal node {:?}.", action, hinfo)
-            }
-        };
-
-        let hist = &hinfo.history;
-
-        let new_state = self.update_state(&hist, &hinfo.state, action);
-
-        let mut new_hist = hist.clone(); // Todo: more efficient copy (prealloc)?
-        new_hist.push(action);
-
-        let new_active = self.active_player(&new_hist, &new_state);
-
-        let mut new_obs = hinfo.observations.clone();
-        if !new_obs.is_empty() {
-            let obs = self.observations(&new_hist, &new_state);
-            for (i, (ref mut ovec, ref o)) in new_obs.iter_mut().zip(obs.iter()).enumerate() {
-                if let ActivePlayer::Player(p, _) = hinfo.active {
-                    if p as usize == i {
-                        ovec.push(PlayerObservation::OwnAction(action));
-                    }
-                }
-                if let Some(oin) = o {
-                    ovec.push(PlayerObservation::Observation(oin.clone()));
-                }
+    fn play(&self, hist: &HistoryInfo<Self>, action_index: usize) -> HistoryInfo<Self> {
+        debug_assert_eq!(hist.observations.len(), self.players() + 1);
+        if let ActivePlayer::Terminal(_) = hist.active {
+            panic!("playing in terminal state {:?}", hist);
+        }
+        let prev_player = hist.active.player();
+        let action = hist.active.actions()
+                         .get(action_index as usize)
+                         .expect("action index outside action list").clone();
+        let history_indices = extended_vec(&hist.history_indices, action_index as ActionIndex);
+        let history = extended_vec(&hist.history, action);
+        // Observation extensions by own action
+        let mut observations = hist.observations.clone();
+        if let Some(p) = prev_player {
+            observations[p].push(PlayerObservation::OwnAction(action_index as u32));
+        }
+        // Game-specific logic
+        let (state, active, obs) = self.update_state(&hist.state, &history, &hist.active);
+        debug_assert_eq!(obs.len(), self.players() + 1);
+        if let ActivePlayer::Player(p, _) = active {
+            debug_assert!((p as usize) < self.players());
+        }
+        // Observation extension by new observed
+        for (ovec, option_ob) in observations.iter_mut().zip(obs) {
+            if let Some(ob) = option_ob {
+                ovec.push(PlayerObservation::Observation(ob))
             }
         }
-
         HistoryInfo {
-            history: new_hist,
-            state: new_state,
-            observations: new_obs,
-            active: new_active,
+            history_indices,
+            history,
+            observations,
+            state,
+            active,
         }
     }
 
-    fn update_state(
-        &self,
-        history: &History,
-        old_state: &Self::StateData,
-        action: ActionIndex,
-    ) -> Self::StateData {
-        Default::default()
+
+    ////////// Alternatively, offer to reuse the state, observations, history, etc.
+    // Reuse variant of play()
+    fn play_owned(&self, hist: HistoryInfo<Self>, action_index: usize) -> HistoryInfo<Self> {
+        unimplemented!()
+    }
+    // Match on Cow
+    fn play_cow(&self, hist: Cow<HistoryInfo<Self>>, action_index: usize) -> HistoryInfo<Self> {
+        match hist {
+            Cow::Borrowed(r) => self.play(r, action_index),
+            Cow::Owned(h) => self.play_owned(h, action_index),
+        }
     }
 
-    fn active_player(&self, history: &History, state: &Self::StateData) -> ActivePlayer;
+    // The player would optionally implement state reuse with (only this instead of update_state)
+    fn update_state_cow(&self, state: Cow<Self::State>, history: &Vec<Self::Action>) -> 
+        (Self::State, ActivePlayer<Self>, Vec<Option<Self::Observation>>)
+    {
+        // If you want to modify an existing state
+        let mut state2 = state.into_owned();
+        // Otherwise just use state as &State via Deref
+        unimplemented!()
+    }
+}
 
-    fn observations(
-        &self,
-        history: &History,
-        state: &Self::StateData,
-    ) -> Vec<Option<Self::Observation>>;
+#[derive(Clone, Hash, Debug, PartialEq, Eq)]
+pub enum PlayerObservation<O: Clone + Hash + Debug + PartialEq + Eq> {
+    OwnAction(ActionIndex),
+    Observation(O),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ActivePlayer<G: Game> {
+    Player(u32, Vec<G::Action>),
+    Chance(Categorical<G::Action>),
+    Terminal(Vec<Utility>),
+}
+
+impl<G: Game> ActivePlayer<G> {
+    pub fn actions<'a>(&'a self) -> &'a [G::Action] {
+        match self {
+            ActivePlayer::Terminal(_) => &[],
+            ActivePlayer::Player(p, ref actions) => actions,
+            ActivePlayer::Chance(ref dist) => dist.items(),
+        }
+    }
+    pub fn player<'a>(&'a self) -> Option<usize> {
+        match self {
+            ActivePlayer::Terminal(_) => None,
+            ActivePlayer::Player(p, _) => Some(*p as usize),
+            ActivePlayer::Chance(_) => None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct HistoryInfo<G: Game> {
+    pub history_indices: Vec<ActionIndex>,
+    pub history: Vec<G::Action>,
+    pub active: ActivePlayer<G>,
+    pub observations: Vec<Vec<PlayerObservation<G::Observation>>>,
+    pub state: G::State,
+}
+
+impl<G: Game> HistoryInfo<G> {
+    pub fn new(game: &G, state: G::State, active: ActivePlayer<G>) -> Self {
+        HistoryInfo {
+            history_indices: Vec::new(),
+            history: Vec::new(),
+            observations: vec!{vec!{}; (game.players() + 1) as usize},
+            state,
+            active,
+        }
+    }
+}
+
+#[inline]
+fn extended_vec<T: Clone>(v: &Vec<T>, val: T) -> Vec<T> {
+    let mut v2 = Vec::with_capacity(v.len() + 1);
+    v2.clone_from(v);
+    v2.push(val);
+    v2
 }
--- src/goofspiel.rs
+++ src/goofspiel.rs
@@ -1,4 +1,4 @@
-use crate::{ActionIndex, ActivePlayer, Game, History, HistoryInfo, Probability, Categorical};
+use crate::{ActionIndex, ActivePlayer, Game, HistoryInfo, Categorical, Utility};
 use bit_set::BitSet;
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
@@ -8,11 +8,12 @@ pub enum Scoring {
     Absolute,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Goofspiel {
     cards: usize,
     scoring: Scoring,
     card_set: BitSet,
+    values: Vec<Utility>,
 }
 
 impl Goofspiel {
@@ -21,67 +22,80 @@ impl Goofspiel {
             cards,
             scoring,
             card_set: (1..cards + 1).collect(),
+            values: (1..cards + 1).map(|x| x as Utility).collect(),
         }
     }
 }
 
-impl Game for Goofspiel {
-    type StateData = ();
-    type Observation = u16;
+/// Players are p0 and p1, p2 is chance
+#[derive(Clone, Debug)]
+pub struct State {
+    cards: [BitSet; 3],
+    scores: [f64; 2],
+}
 
-    fn active_player(&self, history: &History, _state: &Self::StateData) -> ActivePlayer {
-        let len = history.len();
-        if len >= self.cards * 3 {
-            let mut p0 = 0f64;
-            let mut p1 = 0f64;
-            for h in history.chunks(3) {
-                if let [c, c0, c1] = h {
-                    if c0 > c1 {
-                        p0 += *c as f64;
-                    }
-                    if c1 > c0 {
-                        p1 += *c as f64;
-                    }
-                }
-            }
-            let w0 = (p0 - p1).signum();
-            ActivePlayer::Terminal(match self.scoring {
-                Scoring::Absolute => vec![p0, p1],
-                Scoring::WinLoss => vec![w0, -w0],
-                Scoring::ZeroSum => vec![p0 - p1, p1 - p0],
-            })
-        } else {
-            let modulo = len % 3;
-            let mut cards = self.card_set.clone();
-            for i in (modulo..len).step_by(3) {
-                cards.remove(history[i] as usize);
-            }
-            let cards: Vec<_> = cards.iter().map(|c| c as ActionIndex).collect();
-            if modulo == 0 {
-                ActivePlayer::Chance(Categorical::uniform(cards))
-            } else {
-                ActivePlayer::Player((modulo - 1) as u32, cards)
-            }
-        }
+impl Game for Goofspiel {
+    type State = State;
+    type Observation =i32;
+    type Action = u32;
+
+    fn players(&self) -> usize { 2 }
+
+    fn initial_state(&self) -> (Self::State, ActivePlayer<Self>) {
+        let state = State {
+            cards: [self.card_set.clone(), self.card_set.clone(), self.card_set.clone()],
+            scores: [0.0, 0.0],
+        };
+        let active = ActivePlayer::Chance(Categorical::uniform(
+            self.card_set.iter().map(|x| x as u32).collect::<Vec<_>>()));
+        (state, active)
     }
 
-    fn observations(
-        &self,
-        history: &History,
-        _state: &Self::StateData,
-    ) -> Vec<Option<Self::Observation>> {
+    fn update_state(&self, prev_state: &Self::State, history: &Vec<Self::Action>, prev_active: &ActivePlayer<Self>) -> 
+        (Self::State, ActivePlayer<Self>, Vec<Option<Self::Observation>>) {
+        // p=0, p=1 are players p=2 is chance
         let len = history.len();
-        println!("OBS {:?}", history);
-        vec![
-            match len % 3 {
-                1 => Some(history[len - 1]),
-                0 if len >= 3 => Some(
-                    ((history[len - 2] as i16 - history[len - 1] as i16).signum() + 1) as u16
-                ),
-                _ => None,
-            };
-            3
-        ]
+        let prev_player = (history.len() + 1) % 3;
+        let next_player = (history.len() - 1) % 3;
+        let action = *history.last().unwrap();
+        let mut obs = None;
+        // Play the selected card, update state
+        let mut state = prev_state.clone();
+        state.cards[prev_player].remove(action as usize);
+        // Score update and observation
+        if prev_player == 1 {
+            let bet = self.values[(history[len - 3] - 1) as usize];
+            let winner = ((history[len - 2] as i32) - (action as i32)).signum();
+            if winner == 1 {
+                state.scores[0] += bet;
+            }
+            if winner == -1 {
+                state.scores[1] += bet;
+            }
+            obs = Some(winner);
+        }
+        // Observe public card
+        if prev_player == 2 {
+            obs = Some(action as i32);
+        }
+        // Terminal reached or active player
+        let active = if len == self.cards * 3 {
+            let d = state.scores[0] - state.scores[1];
+            ActivePlayer::Terminal(match self.scoring {
+                Scoring::Absolute => state.scores.as_ref().into(),
+                Scoring::ZeroSum => { vec![d, -d] },
+                Scoring::WinLoss => { vec![d.signum(), -d.signum()] }
+            })            
+        } else {
+            let acts = state.cards[next_player].iter().map(|x| x as u32).collect();
+            if next_player == 2 {
+                ActivePlayer::Chance(Categorical::uniform(acts))
+            } else {
+                ActivePlayer::Player(next_player as u32, acts)
+            }
+        };
+        // Return new info triplet
+        (state, active, vec!{obs; 3})
     }
 }
 
@@ -97,60 +111,60 @@ mod test {
             (-4.0, 4.0, Scoring::ZeroSum),
         ] {
             let g = Goofspiel::new(4, *scoring);
-            let mut hist = g.new_history(true);
+            let mut hist = g.start();
             assert_eq!(
                 hist.active,
                 ActivePlayer::Chance(Categorical::uniform(vec![1, 2, 3, 4]))
             );
             for a in &[2, 1, 2, 3, 2, 4, 4, 3, 3, 1, 4, 1] {
-                hist = g.play(&hist, *a);
+                hist = g.play_value(&hist, a);
             }
             assert_eq!(hist.active, ActivePlayer::Terminal(vec![*p0, *p1]));
             assert_eq!(
                 hist.observations[0],
                 vec![
                     Observation(2),
-                    OwnAction(1),
-                    Observation(0),
+                    OwnAction(0),
+                    Observation(-1),
                     Observation(3),
-                    OwnAction(2),
-                    Observation(0),
+                    OwnAction(0),
+                    Observation(-1),
                     Observation(4),
-                    OwnAction(3),
+                    OwnAction(0),
+                    Observation(0),
                     Observation(1),
-                    Observation(1),
-                    OwnAction(4),
-                    Observation(2)
+                    OwnAction(0),
+                    Observation(1)
                 ]
             );
             assert_eq!(
                 hist.observations[1],
                 vec![
                     Observation(2),
-                    OwnAction(2),
-                    Observation(0),
-                    Observation(3),
-                    OwnAction(4),
-                    Observation(0),
-                    Observation(4),
-                    OwnAction(3),
-                    Observation(1),
-                    Observation(1),
                     OwnAction(1),
-                    Observation(2)
+                    Observation(-1),
+                    Observation(3),
+                    OwnAction(2),
+                    Observation(-1),
+                    Observation(4),
+                    OwnAction(1),
+                    Observation(0),
+                    Observation(1),
+                    OwnAction(0),
+                    Observation(1)
                 ]
             );
             assert_eq!(
                 hist.observations[2],
                 vec![
                     Observation(2),
-                    Observation(0),
+                    Observation(-1),
                     Observation(3),
-                    Observation(0),
+                    Observation(-1),
                     Observation(4),
+                    Observation(0),
                     Observation(1),
-                    Observation(1),
-                    Observation(2)
+                    Observation(1)
                 ]
             );
         }
